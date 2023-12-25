4c20cd1a261adfe395cf3a2f2fa412411bce01e2
Abdurahman Abukar <abzabukar99@gmail.com>
2023-12-25 20:58:50 +0000

    Update settings with ec2 public ip

--- citation_project/settings.py
+++ citation_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1'] # Allow *.vercel.app
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '3.10.219.115'] # Allow *.vercel.app
 
 
 # Application definition
